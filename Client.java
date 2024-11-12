7722864c53e28be24a1c4b39f358b833f3d176d3
Sophia Tamburrino <sophia.tamburrino>
2024-11-11 23:21:00 -0500

    constructors created

--- Client.java
+++ Client.java
@@ -1,2 +1,43 @@
+//client code interacts with the server
+import java.util.*;
+import java.io.*;
+import java.net.*;
+import java.time.*;
 
-//
+public class Client {
+    String hostname;
+    int port;
+    Socket clientSocket;
+
+    public Client(String theHost, int thePort) throws IOException{
+        this.hostname = theHost;
+        this.port = thePort;
+        //create new socket?
+        try {
+            clientSocket = new Socket(hostname, port);
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println(e);
+        }
+
+        clientSocket = new Socket(hostname, port);
+    }
+
+    public Socket getSocket() {
+        return clientSocket;
+    }
+
+    public String request(String request) {
+        return null;
+    }
+
+    public void handshake() {
+
+    }
+
+    public void disconnect() {
+
+    }
+     
+    
+}
--- Server.java
+++ Server.java
@@ -1,1 +1,16 @@
+//server code recieves and processes requests from clients
+public class Server {
+    int port;
+    public Server(int thePort) {
+        this.port = thePort;
+    }
+
+    public void serve(int i) {
+
+    }
+
+    public void disconnect() {
+        
+    }
+}
 
